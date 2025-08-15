fa32f2ab7d5c08e99e6b3a1f674c8518df24d542
MateusVasc <mvaraujo2004@gmail.com>
2025-08-15 14:49:24 -0300

    feat: adds code of section 1.2 Command-Line Arguments

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	osargs "github.com/MateusVasc/the-go-programming-language/os-args"
+)
+
+func main() {
+	fmt.Println("The Go Programming Language")
+	fmt.Println("")
+
+	// 1.2
+	fmt.Println("1.2 Command-Line Arguments")
+	osargs.Echo()
+	osargs.RangeEcho()
+}
---
+++ os-args/os-args.go
@@ -0,0 +1,37 @@
+package osargs
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Echo() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
+
+func RangeEcho() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
+
+func JoinEcho() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
+
+// Exercise 1.1: Modify the echo program to also print os.Args[0], the name of the command that invoked it.
+
+// Exercise 1.2: Modify the echo program to print the index and value of each of its arguments, one per line.
+
+// Exercise 1.3: Experiment to measure the difference in running time between our potentially inefficient versions and the one that
+// uses strings.Join. (Section1.6 illustrates part of the time package, and Section 11.4 shows how to write benchmark tests for
+// systematic performance evaluation.)
